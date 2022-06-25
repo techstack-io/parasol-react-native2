28b7b3ff8a4812ec890ece644847c4dcfbb0deb2
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-06-24 23:56:35 -0500

    Added welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -3,16 +3,36 @@ import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 
 function WelcomeScreen(props) {
     return (
+        <>
         <ImageBackground
             style={styles.background}
             source={require('../assets/hero-img-3.png')}
         />
+        <View style={styles.loginButton}>
+
+        </View>
+        </>
+     
     );
 }
 
 const styles = StyleSheet.create({
     background: {
-        flex: 1
+        flex: 1,
+        width: '100%',
+        height: '50%'
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#002869',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 120,
+        elevation: 3,
+   
     }
 })
 
